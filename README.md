0185dfddf3f8526f241e39d031154e4932109942
Snehasish Mondal <snehasishmondal016@gmail.com>
2023-04-29 21:14:19 +0530

    Adding preview images and removing commented out code.

--- README.md
+++ README.md
@@ -6,9 +6,14 @@ Backend:
 Frontend:
     - Thymeleaf
 
-## UI screen
+## Preview
 
-![Chat screen](img/chat_screen.jpg "Chat screen")
+![Registration](preview/registration.PNG "Registration")
+![Login](preview/login.PNG "Login")
+![Admin Login](preview/admin_login.PNG "Admin Login")
+![Tech Lead Login](preview/tech_lead_login.PNG "Tech Lead Login")
+![Developer Login](preview/developer_login.PNG "Developer Login")
+![Index](preview/index.PNG "Index")
 
 To start:   
         - mvn spring-boot:run
--- src/main/java/com/major/project/model/PassGen.java
+++ src/main/java/com/major/project/model/PassGen.java
@@ -6,11 +6,17 @@ public class PassGen {
 
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("ad11234"));
-        System.out.println(encoder.encode("tl11234"));
-        System.out.println(encoder.encode("ad21234"));
-        System.out.println(encoder.encode("tl21234"));
-        System.out.println(encoder.encode("12345678"));
+//        System.out.println(encoder.encode("ad11234"));
+//        System.out.println(encoder.encode("tl11234"));
+//        System.out.println(encoder.encode("ad21234"));
+//        System.out.println(encoder.encode("tl21234"));
+        System.out.println(encoder.encode("aloksharma"));
+
+        /*System.out.println(encoder.matches("12345678","$2a$10$plGVQRlY2err71ku/iDBAO2hjeD0.nltuFnd37dTLnfTA7nf4QL2."));
+        System.out.println(encoder.matches("12345678","$2a$10$eVUGsyFRvvzPCSsfEz3EIe9hOsf0t2.Wub819lUPmEM0cwy2yyt3q"));
+        System.out.println(encoder.matches("12345678","$2a$10$jH/J2pSqfddkaatBEhlxEe755nqKMIcqrAuCvzB6EWw94fyYp7.W2"));
+        System.out.println(encoder.matches("12345678","$2a$10$j0cfcAAcCNQSLh1LJX4mCevq9j1bR1a0TguL882SYAubykpSWdJiW"));*/
+
     }
 
 }
